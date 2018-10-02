905611de2b28f0a93fdb7cba3c3aa9cde6a9dc93
Pedro Mata Rodrigues <pmmatarodrigues@gmail.com>
2018-10-02 11:14:02 +0100

    First Commit

---
+++ src/components/header.js
@@ -0,0 +1,36 @@
+import React from "react"
+import '../styles/styles.css'
+
+class Header extends React.Component {
+    render(){
+        return(
+            <header class="header">
+                <div class="logo">
+                    <img src="https://i.imgur.com/u2RjHiT.png" alt="DeMata"/>
+                </div>
+                <input type="checkbox"/>
+                <span class="menu">
+                    <span class="hamburger">
+                    </span>
+                    <b>MENU</b>
+                </span>
+                <ul>
+                <li>
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <a href="">Profile</a>
+                </li>
+                <li>
+                    <a href="">Skills</a>
+                </li>
+                <li>
+                    <a href="">Goals</a>
+                </li>
+                </ul>    
+            </header>
+        );
+    }
+}
+
+export default Header;
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+import '../styles/styles.css'
+import Header from '../components/header'
+
+export default () => (
+ <Header/>
+)
